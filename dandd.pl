2302583721a4fe85a5e0642f8cbc9cfb8f3b5afa
Ethan House <ewh2048@rit.edu>
2012-05-06 23:25:02 -0400

    Adds up rolls, displays total in parens

--- dandd.pl
+++ dandd.pl
@@ -2,37 +2,60 @@
 use warnings;
 use strict;
 
-# variables
-my $input; # store of user input
 
 # Read, Eval, loop
 while () {
+	# Variables
+	my @array;
+	my $total = 0;
 	# Read
 	print(">>");
-	$input = <>;
+	my $input = <>;
 	chomp($input);
 
 	# Eval
 	last if ($input eq 'quit') || ($input eq 'q') || ($input eq 'exit');
+
 	# Dice Roller
-	$input =~ s/(\d+)?(d)(\d+)/dice($1,$3)/geis;
+	if($input =~ m/(\d+)?(d)(\d+)/){
+		# Replace All Dice rolls
+		$input =~ s/(\d+)?(d)(\d+)/dice($1,$3)/geis;
+		@array = ($input =~ m/(\d+|\+)/g);
+
+		foreach my $counter (0..$#array){
+			if($array[$counter] =~ m/\+/g){
+				$total += $array[$counter - 1];
+				$total += $array[$counter + 1];
+			}
+			if($array[$counter] =~ m/\-/g){
+				$total -= $array[$counter - 1];
+				$total -= $array[$counter + 1];
+			}
+		}
+	}
 
 	# print 
 	if($input){
-		print("$input\n");
+		printf("(%d) %s\n",$total,$input);
+	}
+	if($input eq 'clear'){
+		system('clear');
 	}
 }
+
 sub dice{
 	# $_[0] = num of dice
 	# $_[1] = sides of dice
+
+	my $sides = $_[1];
+	my $roll = 0;
 	my $num_dice;
+
 	if(!defined($_[0])){
 		$num_dice = 1;
 	}else{
 		$num_dice = $_[0];
 	}
-	my $sides = $_[1];
-	my $roll = 0;
 
 	for(my $x=0; $x < $num_dice; $x++){
 		$roll += int(rand($sides))+1;
